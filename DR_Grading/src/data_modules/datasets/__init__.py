db1401c8c0d3479e4ef08f3cc1397a087e106b32
mateuszeiser-coder <mateuszeiser@gmail.com>
2026-01-29 10:02:48 -0300

    Delete DR_Grading directory

--- DR_Grading/src/data_modules/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .grading_dataset import GradingDataset
-
-
-def get_drac_dataset(task, data_dir, split, transform, fold_idx, input_size, u_df=None):
-    return GradingDataset(data_dir, split, transform, fold_idx, input_size, u_df)
